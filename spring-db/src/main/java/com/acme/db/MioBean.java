c31280ebc0df816c9756a0d70c9cf7ba12731e7e
daniele.canteri <daniele.canteri@gmail.com>
2015-07-05 15:17:51 +0200

    utilizzo del jdbctemplate per una count

--- spring-db/src/main/java/com/acme/db/MioBean.java
+++ spring-db/src/main/java/com/acme/db/MioBean.java
@@ -3,6 +3,7 @@ package com.acme.db;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +14,17 @@ public class MioBean {
 
 	public void run() {
 		System.out.println(dataSource);
+
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+		doCount(jdbcTemplate);
+
 	}
-	
-	
+
+	private void doCount(JdbcTemplate jdbcTemplate) {
+		Integer count = jdbcTemplate.queryForObject(
+				"select count(*) from prova", Integer.class);
+		System.out.println("count: " + count);
+	}
+
 }
