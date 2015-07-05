a6ac692e888f0870aa45a5df79dc1d29d08b8921
daniele.canteri <daniele.canteri@gmail.com>
2015-07-05 14:29:31 +0200

    commit iniziale progetto di esempio utilizzo db in spring

---
+++ spring-db/src/main/java/com/acme/db/Main.java
@@ -0,0 +1,15 @@
+package com.acme.db;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("START");
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"beans.xml");
+		System.out.println(applicationContext);
+		System.out.println("END");
+	}
+}
