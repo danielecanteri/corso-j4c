47a625f47f2e980dbe37a87119fa0b69908eb723
daniele.canteri <daniele.canteri@gmail.com>
2015-07-07 13:20:20 +0200

    utilizzo di JdbcCursorItemReader nel batch di prova

--- batch-hr/src/main/java/com/acme/hr/SysoItemWriter.java
+++ batch-hr/src/main/java/com/acme/hr/SysoItemWriter.java
@@ -5,14 +5,16 @@ import java.util.List;
 
 import org.springframework.batch.item.ItemWriter;
 
-public class SysoItemWriter implements ItemWriter<String> {
+import com.acme.hr.dto.Prova;
+
+public class SysoItemWriter implements ItemWriter<Prova> {
 
 	private List<String> list = new ArrayList<String>();
 
-	public void write(List<? extends String> items) throws Exception {
-		for (String string : items) {
-			System.out.println(string);
-			list.add(string);
+	public void write(List<? extends Prova> items) throws Exception {
+		for (Prova prova : items) {
+			System.out.println(prova.getDescrizione());
+			list.add(prova.getDescrizione());
 		}
 	}
 
---
+++ batch-hr/src/main/java/com/acme/hr/dto/Prova.java
@@ -0,0 +1,15 @@
+package com.acme.hr.dto;
+
+public class Prova {
+
+	private String descrizione;
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+}
